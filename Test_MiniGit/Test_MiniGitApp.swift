3a299b267ef7363135d796f6897398d146348562
HuseyinAyt <hueseyin@i-ten.de>
2023-12-10 17:10:33 +0100

    Initial Commit

---
+++ Test_MiniGit/Test_MiniGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_MiniGitApp.swift
+//  Test_MiniGit
+//
+//  Created by MiniHus on 10.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Test_MiniGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
